5320bfcf4afb2faf6142663fdd01d4aaa50023d6
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:42:26 +0700

    #11 - Add OpenAPIConfiguration

---
+++ src/main/java/com/hendisantika/config/OpenAPIConfiguration.java
@@ -0,0 +1,23 @@
+package com.hendisantika.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mysql-redis-rabbitmq
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/22
+ * Time: 21.42
+ */
+@Configuration
+public class OpenAPIConfiguration {
+    @Value("${app.local}")
+    private String localhost;
+
+    @Value("${app.dev}")
+    private String development;
+
+}
