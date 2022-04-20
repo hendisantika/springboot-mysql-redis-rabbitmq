f6b2b156ab91dd30932531862c02e8f7bcd7adea
Hendi Santika <hendisantika@gmail.com>
2022-04-20 22:02:26 +0700

    #44 - Add passwordEncoder Bean

--- src/main/java/com/hendisantika/SpringbootMysqlRedisRabbitmqApplication.java
+++ src/main/java/com/hendisantika/SpringbootMysqlRedisRabbitmqApplication.java
@@ -2,6 +2,9 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringbootMysqlRedisRabbitmqApplication {
@@ -10,4 +13,8 @@ public class SpringbootMysqlRedisRabbitmqApplication {
         SpringApplication.run(SpringbootMysqlRedisRabbitmqApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
