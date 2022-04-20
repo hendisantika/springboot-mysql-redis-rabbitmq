6c18b8bcb5c9a8033bb45eef5bc63a92ac687e05
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:45:15 +0700

    #12 - Modify OpenAPIConfiguration

--- src/main/java/com/hendisantika/config/OpenAPIConfiguration.java
+++ src/main/java/com/hendisantika/config/OpenAPIConfiguration.java
@@ -1,7 +1,11 @@
 package com.hendisantika.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,31 @@ public class OpenAPIConfiguration {
     @Value("${app.dev}")
     private String development;
 
+    @Bean
+    public OpenAPI openAPI() {
+        io.swagger.v3.oas.models.servers.Server localServer = new io.swagger.v3.oas.models.servers.Server();
+        localServer.setDescription("local");
+        localServer.setUrl(localhost);
+
+        io.swagger.v3.oas.models.servers.Server devServer = new io.swagger.v3.oas.models.servers.Server();
+        devServer.setDescription("dev");
+        devServer.setUrl(development);
+
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.info(new io.swagger.v3.oas.models.info.Info()
+                .title("Spring Boot MySQL Redis RabbitMQ")
+                .description(
+                        "Spring Boot MySQL Redis RabbitMQ")
+                .version("1.0.0")
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                        .name("Hendi Santika")
+                        .url("https://linktr.ee/hendisantika")
+                        .email("hendisantika@yahoo.co.id"))
+                .termsOfService("TOC")
+                .license(new io.swagger.v3.oas.models.info.License().name("License").url("https://linktr" +
+                        ".ee/hendisantika")));
+        openAPI.setServers(Arrays.asList(localServer, devServer));
+
+        return openAPI;
+    }
 }
