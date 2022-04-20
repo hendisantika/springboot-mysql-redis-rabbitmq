d425c74a9b8b29079da3c823ae900b0b0b2e05b2
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:58:07 +0700

    #34 - Add createToken in JwtTokenProvider

--- src/main/java/com/hendisantika/security/JwtTokenProvider.java
+++ src/main/java/com/hendisantika/security/JwtTokenProvider.java
@@ -1,11 +1,16 @@
 package com.hendisantika.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +34,20 @@ public class JwtTokenProvider {
     @PostConstruct
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(jwtProperties.getSecretKey().getBytes());
+    }
+
+    public String createToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("roles", roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getValidityInMs());
+
+        return Jwts.builder()//
+                .setClaims(claims)//
+                .setIssuedAt(now)//
+                .setExpiration(validity)//
+                .signWith(SignatureAlgorithm.HS256, secretKey)//
+                .compact();
     }
 }
