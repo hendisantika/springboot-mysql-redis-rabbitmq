0efd881820e8425c4a9bb6d77090363481e693ca
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:54:18 +0700

    #27 - Add ResourceNotFoundException Event

---
+++ src/main/java/com/hendisantika/exception/ResourceNotFoundException.java
@@ -0,0 +1,31 @@
+package com.hendisantika.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mysql-redis-rabbitmq
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/22
+ * Time: 21.54
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 763040204220054875L;
+
+    public ResourceNotFoundException() {
+        super();
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
