0e9c58b2e02318b24d65b515af1a4deb8297cd9c
Hendi Santika <hendisantika@gmail.com>
2022-04-20 22:00:44 +0700

    #40 - Add convertToDatabaseColumn in LocalDateTimeConverter

--- src/main/java/com/hendisantika/util/LocalDateTimeConverter.java
+++ src/main/java/com/hendisantika/util/LocalDateTimeConverter.java
@@ -4,6 +4,7 @@ import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +17,10 @@ import java.time.LocalDateTime;
  */
 @Converter(autoApply = true)
 public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+    @Override
+    public Timestamp convertToDatabaseColumn(LocalDateTime localDateTime) {
+        return Optional.ofNullable(localDateTime)
+                .map(Timestamp::valueOf)
+                .orElse(null);
+    }
 }
