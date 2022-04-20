f67baad9a5f65e0dad4842709eb49f3874f8ebaa
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:56:49 +0700

    #31 - Add JwtTokenFilter

---
+++ src/main/java/com/hendisantika/security/JwtTokenFilter.java
@@ -0,0 +1,41 @@
+package com.hendisantika.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mysql-redis-rabbitmq
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/22
+ * Time: 21.56
+ */
+public class JwtTokenFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+            throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication auth = token != null ? jwtTokenProvider.getAuthentication(token) : null;
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(req, res);
+    }
+}
