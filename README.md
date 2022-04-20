f6e2ea9aa9210fabe5be1583dbd6cfe5bf2d3197
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:04:54 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-mysql-redis-rabbitmq
---
+++ src/main/java/com/hendisantika/SpringbootMysqlRedisRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlRedisRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMysqlRedisRabbitmqApplication.class, args);
+    }
+
+}
