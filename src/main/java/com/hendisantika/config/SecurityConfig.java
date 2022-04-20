ae74ced3da716e9fbaab29f605194d6e212da4ec
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:39:50 +0700

    #8 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.hendisantika.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mysql-redis-rabbitmq
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/22
+ * Time: 21.39
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+}
