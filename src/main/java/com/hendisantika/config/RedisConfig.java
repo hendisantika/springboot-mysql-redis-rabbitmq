1099b3b5df6957a11a51baa28f94a8c55f338ab3
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:38:23 +0700

    #5 - Add RedisConfig

---
+++ src/main/java/com/hendisantika/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mysql-redis-rabbitmq
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/22
+ * Time: 21.38
+ */
+@Configuration
+public class RedisConfig {
+
+}
