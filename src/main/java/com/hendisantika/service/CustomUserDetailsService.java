bbc2193bcdf3ddba19a40c04a7266ea6c31921ef
Hendi Santika <hendisantika@gmail.com>
2022-04-20 22:03:47 +0700

    #47 - Add loadUserByUsername in CustomUserDetailsService

--- src/main/java/com/hendisantika/service/CustomUserDetailsService.java
+++ src/main/java/com/hendisantika/service/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package com.hendisantika.service;
 
 import com.hendisantika.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,5 +21,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     public CustomUserDetailsService(UserRepository users) {
         this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.users.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username: " + username + " not found"));
     }
 }
