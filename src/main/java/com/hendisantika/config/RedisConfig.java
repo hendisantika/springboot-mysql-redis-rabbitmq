9890e1b16c52e4769ca0eb8a156c573102108380
Hendi Santika <hendisantika@gmail.com>
2022-04-20 21:39:21 +0700

    #7 - Add redisTemplate bean in RedisConfig

--- src/main/java/com/hendisantika/config/RedisConfig.java
+++ src/main/java/com/hendisantika/config/RedisConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.clients.jedis.JedisPoolConfig;
 
 /**
@@ -29,5 +32,15 @@ public class RedisConfig {
         connectionFactory.setHostName("localhost");
         connectionFactory.setPort(6379);
         return connectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        final RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
     }
 }
