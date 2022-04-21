96eaf35ae19f9aa9a0b3578d443566e44a511ee2
Hendi Santika <hendisantika@gmail.com>
2022-04-21 07:05:15 +0700

    #62 - Add findOneUser API in UserController

--- src/main/java/com/hendisantika/controller/UserController.java
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -1,10 +1,20 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,6 +33,33 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User Controller", description = "API to manage User Controller")
 public class UserController extends AbstractRestHandler {
 
-    @Api(tags = {"users"})
     private final UserService userService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(
+            summary = "Get a single user.",
+            description = "Get a single user. You have to provide a valid user ID.",
+            tags = {"Users"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            User.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not Authorized", responseCode = "401",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Forbidden", responseCode = "403",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
+    public User findOneUser(@Parameter(name = "The ID of the user.", required = true) @PathVariable("id") Long id) {
+        log.info("User UserController {}", userService.findUserById(id));
+        return userService.findUserById(id);
+    }
 }
